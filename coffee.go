1acd37b9f3f2540f7f9e52d25df37aa8875eeda5
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-10-28 11:21:00 +0100

    round 3

--- coffee.go
+++ coffee.go
@@ -8,23 +8,26 @@ import (
 type Order struct {
   Code string
   Price int
+  IsExtraHot bool
 }
 
 func getBeverages() (map[string]Order) {
   var beverages map[string]Order
   beverages = make(map[string]Order)
-  beverages["Chocolate"] = Order{"H",50}
-  beverages["Coffee"] = Order{"C",60}
-  beverages["Tea"] = Order{"T",40}
+  beverages["Chocolate"] = Order{"H",50, true}
+  beverages["Coffee"] = Order{"C",60, true}
+  beverages["Tea"] = Order{"T",40, true}
+  beverages["Orange"] = Order{"O",60, false}
   return beverages
 }
 
-func PadHasBeenPressed(beverageString string, numberOfSugar int, money int) string {
+func PadHasBeenPressed(beverageString string, numberOfSugar int, money int, extraHotRequest bool) string {
   beverage := getBeverages()[beverageString]
   difference := beverage.Price - money
   if (difference > 0) {
     return fmt.Sprintf("M: Missing %dc", difference)
   }
+  extraHot := ""
   touillette := ""
   sugarCode := ""
   if (numberOfSugar > 0) {
@@ -34,5 +37,8 @@ func PadHasBeenPressed(beverageString string, numberOfSugar int, money int) stri
     touillette = "0"
     sugarCode =  strconv.Itoa(numberOfSugar)
   }
-  return fmt.Sprintf("%s:%s:%s", beverage.Code, sugarCode, touillette)
+  if (extraHotRequest && beverage.IsExtraHot) {
+    extraHot = "h"
+  }
+  return fmt.Sprintf("%s%s:%s:%s", beverage.Code, extraHot, sugarCode, touillette)
 }
--- coffee_test.go
+++ coffee_test.go
@@ -6,42 +6,63 @@ import (
 )
 
 func TestOrderAChocolate(t *testing.T) {
-  equals(PadHasBeenPressed("Chocolate",0, 100),"H::", t)
+  equals(PadHasBeenPressed("Chocolate",0, 100, false),"H::", t)
 }
 
 func TestOrderACoffee(t *testing.T) {
-  equals(PadHasBeenPressed("Coffee",0, 100),"C::", t)
+  equals(PadHasBeenPressed("Coffee",0, 100, false),"C::", t)
 }
 
 func TestOrderATea(t *testing.T) {
-  equals(PadHasBeenPressed("Tea", 0, 100),"T::", t)
+  equals(PadHasBeenPressed("Tea", 0, 100, false),"T::", t)
 }
 
 func TestOrderOneSugar(t *testing.T) {
-  equals(PadHasBeenPressed("Chocolate",1, 100),"H:1:0", t)
+  equals(PadHasBeenPressed("Chocolate",1, 100, false),"H:1:0", t)
 }
 
 func TestOrderTwoSugar(t *testing.T) {
-  equals(PadHasBeenPressed("Chocolate",2, 100),"H:2:0", t)
+  equals(PadHasBeenPressed("Chocolate",2, 100, false),"H:2:0", t)
 }
 
 func TestOrderThreeSugar(t *testing.T) {
-  equals(PadHasBeenPressed("Chocolate",3, 100),"H:2:0", t)
+  equals(PadHasBeenPressed("Chocolate",3, 100, false),"H:2:0", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnough(t *testing.T) {
-  equals(PadHasBeenPressed("Chocolate",0, 50),"H::", t)
-  equals(PadHasBeenPressed("Chocolate",0, 40),"M: Missing 10c", t)
+  equals(PadHasBeenPressed("Chocolate",0, 50, false),"H::", t)
+  equals(PadHasBeenPressed("Chocolate",0, 40, false),"M: Missing 10c", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnoughForTea(t *testing.T) {
-  equals(PadHasBeenPressed("Tea",0, 40),"T::", t)
-  equals(PadHasBeenPressed("Tea",0, 30),"M: Missing 10c", t)
+  equals(PadHasBeenPressed("Tea",0, 40, false),"T::", t)
+  equals(PadHasBeenPressed("Tea",0, 30, false),"M: Missing 10c", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnoughForCoffee(t *testing.T) {
-  equals(PadHasBeenPressed("Coffee",0, 60),"C::", t)
-  equals(PadHasBeenPressed("Coffee",0, 50),"M: Missing 10c", t)
+  equals(PadHasBeenPressed("Coffee",0, 60, false),"C::", t)
+  equals(PadHasBeenPressed("Coffee",0, 50, false),"M: Missing 10c", t)
+}
+
+func TestDontAllowOrderIfMoneyIfNotEnoughForOrange(t *testing.T) {
+  equals(PadHasBeenPressed("Orange",0, 60, false),"O::", t)
+  equals(PadHasBeenPressed("Orange",0, 50, false),"M: Missing 10c", t)
+}
+
+func TestOrderAnExtraHotChocolate(t *testing.T) {
+  equals(PadHasBeenPressed("Chocolate",0, 100, true),"Hh::", t)
+}
+
+func TestOrderAnExtraHotCoffee(t *testing.T) {
+  equals(PadHasBeenPressed("Coffee",0, 100, true),"Ch::", t)
+}
+
+func TestOrderAnExtraHotTea(t *testing.T) {
+  equals(PadHasBeenPressed("Tea", 0, 100, true),"Th::", t)
+}
+
+func TestOrderAnExtraHotOrange(t *testing.T) {
+  equals(PadHasBeenPressed("Orange", 0, 100, true),"O::", t)
 }
 
 func equals(value string, expected string, t *testing.T) {
