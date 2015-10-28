b909b368e3666b31d19e3fbd481daf4fb3bb757d
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-10-28 13:08:00 +0100

    Big Refactoring

--- coffee.go
+++ coffee.go
@@ -7,6 +7,29 @@ import (
   "sort"
 )
 
+type Beverages struct {
+  Sum int
+  List map[string]*Beverage
+}
+
+func (b *Beverages) Record(beverage *Beverage) {
+  beverage.Record()
+  b.Sum += beverage.Price
+}
+
+func (b *Beverages) Get(name string) *Beverage{
+    return b.List[name]
+}
+
+func (b *Beverages) Init() {
+  b.Sum = 0
+  b.List = make(map[string]*Beverage)
+  b.List["Chocolate"] = &Beverage{"H",50, true, 0, "Chocolate"}
+  b.List["Coffee"] = &Beverage{"C",60, true, 0, "Coffee"}
+  b.List["Tea"] = &Beverage{"T",40, true, 0, "Tea"}
+  b.List["Orange"] = &Beverage{"O",60, false, 0, "Orange"}
+}
+
 type Beverage struct {
   Code string
   Price int
@@ -19,35 +42,17 @@ func (b *Beverage) Record() {
   b.Count++
 }
 
-var beverages = initBeverages()
-var sum = 0
-
-func initBeverages() (map[string]*Beverage) {
-  beveragesMap := make(map[string]*Beverage)
-  beveragesMap["Chocolate"] = &Beverage{"H",50, true, 0, "Chocolate"}
-  beveragesMap["Coffee"] = &Beverage{"C",60, true, 0, "Coffee"}
-  beveragesMap["Tea"] = &Beverage{"T",40, true, 0, "Tea"}
-  beveragesMap["Orange"] = &Beverage{"O",60, false, 0, "Orange"}
-  return beveragesMap
-}
-
-func Reset() {
-  for _, beverage := range beverages {
-    beverage.Count = 0
-  }
-  sum = 0
-}
+var beverages = new(Beverages)
 
 func PadHasBeenPressed(beverageString string, numberOfSugar int, money int, extraHotRequest bool) string {
-  beverage := beverages[beverageString]
+  beverage := beverages.Get(beverageString)
 
   difference := beverage.Price - money
   if (difference > 0) {
     return fmt.Sprintf("M: Missing %dc", difference)
   }
 
-  beverage.Record()
-  sum += beverage.Price
+  beverages.Record(beverage)
 
   extraHot := handleExtraHot(*beverage, extraHotRequest)
   sugarCode, touillette := handleSugarAndTouillette(*beverage, numberOfSugar)
@@ -57,11 +62,11 @@ func PadHasBeenPressed(beverageString string, numberOfSugar int, money int, extr
 
 func printReport() string {
   myStrings := make([]string,0)
-  for _, beverage := range beverages {
+  for _, beverage := range beverages.List {
     myStrings = append(myStrings, fmt.Sprintf("%s: %d", beverage.Name, beverage.Count))
   }
   sort.Strings(myStrings)
-  myStrings = append(myStrings, fmt.Sprintf(" ,Money: %d", sum))
+  myStrings = append(myStrings, fmt.Sprintf(" ,Money: %d", beverages.Sum))
   return strings.Join(myStrings, ", ")
 }
 
--- coffee_test.go
+++ coffee_test.go
@@ -7,67 +7,81 @@ import (
 )
 
 func TestOrderAChocolate(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Chocolate",0, 100, false),"H::", t)
 }
 
 func TestOrderACoffee(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Coffee",0, 100, false),"C::", t)
 }
 
 func TestOrderATea(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Tea", 0, 100, false),"T::", t)
 }
 
 func TestOrderOneSugar(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Chocolate",1, 100, false),"H:1:0", t)
 }
 
 func TestOrderTwoSugar(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Chocolate",2, 100, false),"H:2:0", t)
 }
 
 func TestOrderThreeSugar(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Chocolate",3, 100, false),"H:2:0", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnough(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Chocolate",0, 50, false),"H::", t)
   equals(PadHasBeenPressed("Chocolate",0, 40, false),"M: Missing 10c", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnoughForTea(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Tea",0, 40, false),"T::", t)
   equals(PadHasBeenPressed("Tea",0, 30, false),"M: Missing 10c", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnoughForCoffee(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Coffee",0, 60, false),"C::", t)
   equals(PadHasBeenPressed("Coffee",0, 50, false),"M: Missing 10c", t)
 }
 
 func TestDontAllowOrderIfMoneyIfNotEnoughForOrange(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Orange",0, 60, false),"O::", t)
   equals(PadHasBeenPressed("Orange",0, 50, false),"M: Missing 10c", t)
 }
 
 func TestOrderAnExtraHotChocolate(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Chocolate",0, 100, true),"Hh::", t)
 }
 
 func TestOrderAnExtraHotCoffee(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Coffee",0, 100, true),"Ch::", t)
 }
 
 func TestOrderAnExtraHotTea(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Tea", 0, 100, true),"Th::", t)
 }
 
 func TestOrderAnExtraHotOrange(t *testing.T) {
+  beverages.Init()
   equals(PadHasBeenPressed("Orange", 0, 100, true),"O::", t)
 }
 
 func TestPrintReport(t *testing.T) {
-  Reset()
+  beverages.Init()
   PadHasBeenPressed("Orange", 0, 100, false)
   PadHasBeenPressed("Orange", 0, 100, false)
   PadHasBeenPressed("Orange", 0, 100, false)
@@ -78,7 +92,7 @@ func TestPrintReport(t *testing.T) {
 }
 
 func TestPrintReportWithMoney(t *testing.T) {
-  Reset()
+  beverages.Init()
   PadHasBeenPressed("Orange", 0, 100, false)
   PadHasBeenPressed("Tea", 0, 100, false)
   PadHasBeenPressed("Coffee", 0, 100, false)
