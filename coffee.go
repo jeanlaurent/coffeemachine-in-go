4aaef773186f9200fb7d7e7f0c839a95b4f8ee0c
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-10-28 11:27:43 +0100

    Refactor FTW

--- coffee.go
+++ coffee.go
@@ -5,25 +5,25 @@ import (
   "strconv"
 )
 
-type Order struct {
+type Beverage struct {
   Code string
   Price int
   IsExtraHot bool
 }
 
-func getBeverages() (map[string]Order) {
-  var beverages map[string]Order
-  beverages = make(map[string]Order)
-  beverages["Chocolate"] = Order{"H",50, true}
-  beverages["Coffee"] = Order{"C",60, true}
-  beverages["Tea"] = Order{"T",40, true}
-  beverages["Orange"] = Order{"O",60, false}
+func getBeverages() (map[string]Beverage) {
+  var beverages map[string]Beverage
+  beverages = make(map[string]Beverage)
+  beverages["Chocolate"] = Beverage{"H",50, true}
+  beverages["Coffee"] = Beverage{"C",60, true}
+  beverages["Tea"] = Beverage{"T",40, true}
+  beverages["Orange"] = Beverage{"O",60, false}
   return beverages
 }
 
 func PadHasBeenPressed(beverageString string, numberOfSugar int, money int, extraHotRequest bool) string {
   beverage := getBeverages()[beverageString]
-  
+
   difference := beverage.Price - money
   if (difference > 0) {
     return fmt.Sprintf("M: Missing %dc", difference)
@@ -35,7 +35,7 @@ func PadHasBeenPressed(beverageString string, numberOfSugar int, money int, extr
   return fmt.Sprintf("%s%s:%s:%s", beverage.Code, extraHot, sugarCode, touillette)
 }
 
-func handleSugarAndTouillette(beverage Order, numberOfSugar int) (string, string) {
+func handleSugarAndTouillette(beverage Beverage, numberOfSugar int) (string, string) {
   if (numberOfSugar > 0) {
     if (numberOfSugar > 2) {
       numberOfSugar = 2
@@ -45,7 +45,7 @@ func handleSugarAndTouillette(beverage Order, numberOfSugar int) (string, string
   return "",""
 }
 
-func handleExtraHot(beverage Order, extraHotRequest bool) string {
+func handleExtraHot(beverage Beverage, extraHotRequest bool) string {
   if (extraHotRequest && beverage.IsExtraHot) {
     return "h"
   }
